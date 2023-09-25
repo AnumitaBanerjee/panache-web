7f1dae7e8b5d7ddcd8447946e529faafe50d66a3
Surajit Dhar <surajitd@indusnet.co.in>
2023-09-25 11:32:10 +0530

    Sidebar Styling

--- components/layout/sidebar/index.js
+++ components/layout/sidebar/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from "next/image";
 import Link from "next/link";
-import Image from 'next/image';
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
 
 const Sidebar = () => {
+
+    // Menu
+    const [menuClass, setMenuClass] = useState(false);
+    const router = useRouter();
+
+
     return (
         <>
             <div className="sidebar-panel">
@@ -17,98 +25,170 @@ const Sidebar = () => {
                 </div>
                 <div className="sidebar-menu">
                     <ul>
-                        <li>
+                        <li className={router.pathname == "/dashboard" ? "active" : ""}>
                             <Link href="/dashboard" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/dashboard-icon.png`}
-                                    alt="logo"
+                                    alt="Dashboard"
                                     width={22}
                                     height={22}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/dashboard-icon-a.png`}
+                                    alt="Dashboard"
+                                    width={22}
+                                    height={22}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Dashboard</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/application-lists" ? "active" : ""}>
+                            <Link href="/application-lists" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/application-list-icon.png`}
-                                    alt="logo"
-                                    width={22}
+                                    alt="Application"
+                                    width={24}
                                     height={22}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/application-list-icon-a.png`}
+                                    alt="Application"
+                                    width={24}
+                                    height={22}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Application Lists</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/university" ? "active" : ""}>
+                            <Link href="/university" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/university-list-icon.png`}
-                                    alt="logo"
-                                    width={22}
-                                    height={22}
+                                    alt="University"
+                                    width={26}
+                                    height={26}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/university-list-icon-a.png`}
+                                    alt="University"
+                                    width={26}
+                                    height={26}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>University Lists</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/recommended" ? "active" : ""}>
+                            <Link href="/recommended" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/recommended-icon.png`}
-                                    alt="logo"
-                                    width={22}
-                                    height={22}
+                                    alt="Recommended"
+                                    width={27}
+                                    height={26}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/recommended-icon-a.png`}
+                                    alt="Recommended"
+                                    width={27}
+                                    height={26}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Recommended Programs</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/pending" ? "active" : ""}>
+                            <Link href="/pending" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/pending-icon.png`}
-                                    alt="logo"
-                                    width={22}
-                                    height={22}
+                                    alt="Pending"
+                                    width={25}
+                                    height={25}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/pending-icon-a.png`}
+                                    alt="Pending"
+                                    width={25}
+                                    height={25}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Pending Tasks</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/financial" ? "active" : ""}>
+                            <Link href="/financial" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/financial-icon.png`}
-                                    alt="logo"
-                                    width={22}
-                                    height={22}
+                                    alt="Financial"
+                                    width={23}
+                                    height={27}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/financial-icon-a.png`}
+                                    alt="Financial"
+                                    width={23}
+                                    height={27}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Financial Aid Information</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/chat" ? "active" : ""}>
+                            <Link href="/chat" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/chat-icon.png`}
-                                    alt="logo"
-                                    width={22}
-                                    height={22}
+                                    alt="Chat"
+                                    width={24}
+                                    height={24}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/chat-icon-a.png`}
+                                    alt="Chat"
+                                    width={24}
+                                    height={24}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Chat</span>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/" className="">
+                        <li className={router.pathname == "/account" ? "active" : ""}>
+                            <Link href="/account" className="">
                                 <Image
                                     src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/account-icon.png`}
-                                    alt="logo"
-                                    width={22}
+                                    alt="Account"
+                                    width={18}
                                     height={22}
                                     priority={true}
+                                    className="img"
+                                />
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/account-icon-a.png`}
+                                    alt="Account"
+                                    width={18}
+                                    height={22}
+                                    priority={true}
+                                    className="img-active"
                                 />
                                 <span>Account Settings</span>
                             </Link>
@@ -117,10 +197,19 @@ const Sidebar = () => {
                     <div className="logout-pane">
                         <Image
                             src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/logout-icon.png`}
-                            alt="logo"
+                            alt="Logout"
                             width={22}
                             height={22}
                             priority={true}
+                            className="img"
+                        />
+                        <Image
+                            src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}/logout-icon-a.png`}
+                            alt="Logout"
+                            width={22}
+                            height={22}
+                            priority={true}
+                            className="img-active"
                         />
                         <span>Logout</span>
                     </div>
